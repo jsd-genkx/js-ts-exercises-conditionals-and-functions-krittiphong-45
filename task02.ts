4a66ca6236772bb933247237eca45bb994caa1c0
krittiphong-45 <krittiphong.cpp@gmail.com>
2025-11-19 15:52:03 +0700

    complete:tasK01-02.ts

--- task02.ts
+++ task02.ts
@@ -1,5 +1,11 @@
 // TODO-02: Use if/else to check if it's a number
 // (hint: typeof or isNaN)
 
-const userInput = "42";
+const userInput = "";
 // Try changing to "hello", 10, null, NaN
+if(typeof userInput === "number" && !isNaN(userInput)){
+  console.log("it's a number");
+}
+else{
+  console.log("it's not a number");
+}
